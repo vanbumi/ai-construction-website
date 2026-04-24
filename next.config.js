e93813e06eae5301f4efc08da771f2d4d793c274
vanbumi <jsp.dio@gmail.com>
2026-04-24 20:16:28 +0700

    fix: next.config.js and stable dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, // untuk deployment static
+    unoptimized: true, // Untuk deployment static
   },
   output: 'standalone',
 }
